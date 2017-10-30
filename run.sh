714802ebad470b59192b05b38175a51877edf5b8
arnoldliaoILMN <aliao@illumina.com>
2017-10-30 16:58:13 -0700

    find gz
    gz

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo hi
 echo it is 945am
 echo project_dir is $project_dir
 
-vcf=`find /data/input -name "*.vcf"`
+vcf=`find /data/input -name "*.vcf.gz"`
 echo found vcf ${vcf[@]}
 echo listing root gitapp
 ls /root
@@ -16,7 +16,7 @@ ls /root/gitapp/filter_SM_from_vcf.pl
 cd /root/gitapp
 # try this find . -name \*.pdf | xargs --max-args=1 --max-procs=$cpus  pdf2ps
 # from https://stackoverflow.com/questions/38160/parallelize-bash-script-with-maximum-number-of-processes
-find /data/input -name "*.vcf" | xargs --max-args=1 --max-procs=16 /root/gitapp/wrap.sh 
+find /data/input -name "*.vcf.gz" | xargs --max-args=1 --max-procs=16 /root/gitapp/wrap.sh 
 <<COMMENT
 for i in ${vcf[@]}
 do 
