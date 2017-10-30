3b278f7cd1414a9d69b95f2d0b7040058721fa94
arnoldliaoILMN <aliao@illumina.com>
2017-10-29 20:06:25 -0700

    fix error
    fixed

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo hi
 echo it is 2pm
 
 echo project_dir is $project_dir
-for i in ` find /data/input "*.vcf*"; 
+for i in ` find /data/input "*.vcf" `
 do 
   filename=`basename $i|sed -e "s/vcf/filtered.vcf/"`
   perl /root/gitapp/filter_SM_from_vcf.pl $i $project_dir/$filename
