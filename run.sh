e205312353bd0223f9198483aa5bc5ae6746016e
arnoldliaoILMN <aliao@illumina.com>
2017-10-30 09:54:14 -0700

    test
    test

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 source /variables.txt
+echo from git run.sh variables are
+cat /variables.txt
 echo hi
-echo it is 2pm
-
+echo it is 945am
 echo project_dir is $project_dir
-for i in ` find /data/input "*.vcf" `
+
+vcf=`find /data/input "*.vcf"`
+echo found vcf ${vcf[@]}
+echo listing root gitapp
+ls /root
+ls /root/gitapp
+
+
+for i in ${vcf[@]}
 do 
-  filename=`basename $i|sed -e "s/vcf/filtered.vcf/"`
+  filename=`basename $i`
+  new_filename=`echo $filename |sed -e "s/vcf/filtered.vcf/"`
   perl /root/gitapp/filter_SM_from_vcf.pl $i $project_dir/$filename
 done
