26f7321518064234e5be326cc1590d2be4f2a631
arnoldliaoILMN <aliao@illumina.com>
2017-10-31 00:57:32 -0700

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -2,21 +2,16 @@ vcfgz=$1
 project_dir=/data/scratch/parsed
 echo project_dir is $project_dir
 echo vcfgz is $vcfgz
-filename=`basename $vcfgz|sed -e "s/vcf.gz/vcf/"`
-new_filename=`echo $filename |sed -e "s/vcf/filtered.vcf/"`
+filename=`basename $vcfgz|sed -e "s/vcf.gz/filtered.vcf/"`
+#new_filename=`echo $filename |sed -e "s/vcf/filtered.vcf/"`
 echo filename is $filename
-echo new_filename $new_filename
+#echo new_filename $new_filename
 
-echo looking at where I am and files
+cd /root/gitapp
 pwd
-ls /root/gitapp
-
-zcat $vcfgz > $project_dir/$filename
-perl /root/gitapp/filter_SM_from_vcf.pl $project_dir/$filename $project_dir/$new_filename
+zcat $vcfgz |perl /root/gitapp/filter_SM_from_vcf.pl - $project_dir/$filename
 
 echo looking at project_dir $project_dir
-
 ls $project_dir
 
-echo 
 
