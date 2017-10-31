3143ea77b1c90f6f5f15f721e987b69cf6b1b63f
arnoldliaoILMN <aliao@illumina.com>
2017-10-30 23:40:14 -0700

    /data/scratch/parsed
    /data/scratch/parsed

--- run.sh
+++ run.sh
@@ -18,15 +18,7 @@ chmod 777 /root/gitapp/wrap.sh
 # try this find . -name \*.pdf | xargs --max-args=1 --max-procs=$cpus  pdf2ps
 # from https://stackoverflow.com/questions/38160/parallelize-bash-script-with-maximum-number-of-processes
 find /data/input -name "*.vcf.gz" | xargs --max-args=1 --max-procs=16 /root/gitapp/wrap.sh 
-<<COMMENT
-for i in ${vcf[@]}
-do 
-  filename=`basename $i`
-  new_filename=`echo $filename |sed -e "s/vcf/filtered.vcf/"`
-  #echo perl /root/gitapp/filter_SM_from_vcf.pl $i $project_dir/$new_filename
-  zcat $i | perl /root/gitapp/filter_SM_from_vcf.pl - $project_dir/$new_filename
-done
-COMMENT
-mv /scratch/parsed $project_dir
+
+mv /data/scratch/parsed $project_dir
 echo done
 
