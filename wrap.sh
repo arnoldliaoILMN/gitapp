a8e26db81ceeae6a751910b84e5e756491e5e808
arnoldliaoILMN <aliao@illumina.com>
2017-10-30 22:30:24 -0700

    te
    te

--- wrap.sh
+++ wrap.sh
@@ -2,12 +2,21 @@ source /variables.txt
 vcfgz=$1
 echo project_dir is $project_dir
 echo vcfgz is $vcfgz
-filename=`basename $vcfgz|sed -e "s/vcf.gz/filtered.vcf/"`
+filename=`basename $vcfgz|sed -e "s/vcf.gz/vcf/"`
+new_filename=`echo $filename |sed -e "s/vcf/filtered.vcf/"`
 echo filename is $filename
+echo new_filename $new_filename
 
 echo looking at where I am and files
 pwd
 ls /root/gitapp
 
-zcat $vcfgz | perl /root/gitapp/filter_SM_from_vcf.pl - $project_dir/$filename
+zcat $vcfgz > $project_dir/$filename
+perl /root/gitapp/filter_SM_from_vcf.pl $project_dir/$filename $project_dir/$new_filename
+
+echo looking at project dir
+
+ls $project_dir
+
+echo 
 
