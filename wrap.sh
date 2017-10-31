579de32f26a0a3d34c5e16bb3e1d364bbc762265
arnoldliaoILMN <aliao@illumina.com>
2017-10-30 17:01:15 -0700

    d
    d

--- wrap.sh
+++ wrap.sh
@@ -1,5 +1,13 @@
 source /variables.txt 
 $vcfgz=$1
+echo project_dir is $project_dir
+echo vcfgz is $vcfgz
 filename=`basename $vcfgz|sed -e "s/vcf.gz/filtered.vcf/"`
+echo filename is $filename
+
+echo looking at where I am and files
+pwd
+ls /root/gitapp
+
 zcat $vcfgz | perl /gitapp/filter_SM_from_vcf.pl - $project_dir/$filename
 
