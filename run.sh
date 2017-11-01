1cab670937ab0fba35adee950fa0d2e5ad62550f
arnoldliaoILMN <aliao@illumina.com>
2017-11-01 14:06:15 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ chmod 777 /root/gitapp/wrap.sh
 # try this find . -name \*.pdf | xargs --max-args=1 --max-procs=$cpus  pdf2ps
 # from https://stackoverflow.com/questions/38160/parallelize-bash-script-with-maximum-number-of-processes
 mkdir -p /data/scratch/parsed
-find /data/input -name "*.vcf.gz" | xargs --max-args=1 --max-procs=16 /root/gitapp/wrap.sh 
+find /data/input -name "*somatic.vcf.gz" | xargs --max-args=1 --max-procs=16 /root/gitapp/wrap.sh 
 bash /txtbox.sh
 mv /data/scratch/parsed $project_dir
 echo done
