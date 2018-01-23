542195e7a4796567fdbf5bb4fdee3dbb11558eb4
arnoldliaoILMN <aliao@illumina.com>
2018-01-23 15:02:11 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,8 +29,9 @@ find /data/input -name "*somatic.SV.vcf*" | xargs -d '\n' --max-args=1 -I '{}' -
 #bash /txtbox.sh
 #mv /data/scratch/parsed $project_dir
 #mv /data/scratch/SV $project_dir
-#cp -rf /genomes $project_dir
 mkdir $project_dir
-find /genomes -name * > $project_dir/genomes.txt
+cp -rf /genomes/Homo_sapiens/NCBI/GRCh38 $project_dir
+
+#find /genomes -name * > $project_dir/genomes.txt
 echo done run.sh
 
